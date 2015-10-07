2be8b75e31010ba6b067c8a8f4a26b6df68137fc
scls19fr <s.celles@gmail.com>
2015-10-07 19:08:50 +0200

    Fix setup and update version number

---
+++ constraint/version.py
@@ -0,0 +1,8 @@
+__author__ = "Gustavo Niemeyer"
+__copyright__ = "Copyright (c) 2005-2014 - Gustavo Niemeyer <gustavo@niemeyer.net>"
+__credits__ = ["Sebastien Celles"]
+__license__ = ""
+__version__ = "1.3"
+__email__ = "gustavo@niemeyer.net"
+__status__ = "Development"
+__url__ = 'https://github.com/python-constraint/python-constraint'
