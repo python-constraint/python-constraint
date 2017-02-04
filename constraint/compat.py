d741f6fca01270a861d26947e0efd6c4092bf6ae
Eduardo Moguillansky <eduardo.moguillansky@gmail.com>
2017-02-04 22:36:36 +0100

    python 3

--- constraint/compat.py
+++ constraint/compat.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import absolute_import
 import sys
+import six
 
 PY2 = sys.version_info[0] == 2
 PY3 = (sys.version_info[0] >= 3)
@@ -10,5 +12,5 @@ if PY3:
     string_types = str
     xrange = range
 else:
-    string_types = basestring  # noqa
+    string_types = six.string_types  # noqa
     xrange = xrange
