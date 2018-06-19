d22148c91fc61ffb4e8e8839bffb8724269555c9
Matt Kindy <35696388+mattkindy-praetorian@users.noreply.github.com>
2018-06-18 19:53:03 -0500

    Convert `dict_keys` object to list for python3 support

--- constraint/__init__.py
+++ constraint/__init__.py
@@ -688,7 +688,7 @@ class MinConflictsSolver(Solver):
             assignments[variable] = random.choice(domains[variable])
         for _ in xrange(self._steps):
             conflicted = False
-            lst = domains.keys()
+            lst = list(domains.keys())
             random.shuffle(lst)
             for variable in lst:
                 # Check if variable is not in conflict
