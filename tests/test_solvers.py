af2c05871aaf23be10baa66620c4e97ef552747b
Matt Kindy <matt.kindy@praetorian.com>
2018-06-19 07:37:58 -0500

    Fixing based on random nature of MinConflictsSolver

--- tests/test_solvers.py
+++ tests/test_solvers.py
@@ -7,4 +7,11 @@ def test_min_conflicts_solver():
     problem.addVariable("y", [0, 1])
     solution = problem.getSolution()
 
-    assert solution == {'x': 0, 'y': 0}
+    possible_solutions = [
+        {'x': 0, 'y': 0},
+        {'x': 0, 'y': 1},
+        {'x': 1, 'y': 0},
+        {'x': 1, 'y': 1}
+    ]
+
+    assert solution in possible_solutions
