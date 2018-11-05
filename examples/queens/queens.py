64176e68c124467a5428e6a86f2434f4128bf7c5
scls19fr <s.celles@gmail.com>
2018-11-05 09:15:49 +0100

    PEP8

--- examples/queens/queens.py
+++ examples/queens/queens.py
@@ -16,7 +16,7 @@ def solve():
         for col2 in cols:
             if col1 < col2:
                 problem.addConstraint(
-                    lambda row1, row2, col1=col1, 
+                    lambda row1, row2, col1=col1,
                     col2=col2: abs(row1 - row2) !=
                     abs(col1 - col2) and row1 != row2,
                     (col1, col2),
