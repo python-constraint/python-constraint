a0e256117c77365bf0b648bb8521398661778f46
cgohlke <cjgohlke@gmail.com>
2018-12-05 13:49:38 -0800

    Do not install examples in site-packages

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     keywords="csp constraint solving problems problem solver",
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
+    packages=find_packages(exclude=["contrib", "docs", "tests*", "examples*"]),
     # List run-time dependencies here.  These will be installed by pip when your
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
