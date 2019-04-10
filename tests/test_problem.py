22dbd395f4431b33691b6b0b48a7e99e128adae6
Will Hartzell-Baird <WillDHB@gmail.com>
2019-04-09 21:51:50 -0400

    [Fix #50] Fix support for using a Domain subclass

---
+++ tests/test_problem.py
@@ -0,0 +1,28 @@
+from constraint import Constraint, Domain, Problem
+
+
+def test_addVariable_support_domain_subclasses():
+    class MyCustomDomain(Domain):
+        pass
+
+    class MyConstraint(Constraint):
+        def __call__(self, variables, domains,
+                     assignments, forwardcheck=False):
+            assert isinstance(domains['x'], Domain)
+            assert isinstance(domains['y'], MyCustomDomain)
+            return True
+
+    problem = Problem()
+    problem.addVariable("x", [0, 1])
+    problem.addVariable("y", MyCustomDomain([0, 1]))
+    problem.addConstraint(MyConstraint())
+    solution = problem.getSolution()
+
+    possible_solutions = [
+        {"x": 0, "y": 0},
+        {"x": 0, "y": 1},
+        {"x": 1, "y": 0},
+        {"x": 1, "y": 1},
+    ]
+
+    assert solution in possible_solutions
