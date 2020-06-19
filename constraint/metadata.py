66ecbed2e3ee073a07597350e1347b477cd56b2b
KOLANICH <kolan_n@mail.ru>
2020-06-19 16:03:05 +0300

    Moved the metadata into setup.cfg

--- constraint/metadata.py
+++ constraint/metadata.py
@@ -1,8 +1,9 @@
+from .version import __version__
+
 __author__ = "Gustavo Niemeyer"
 __copyright__ = "Copyright (c) 2005-2018 - Gustavo Niemeyer <gustavo@niemeyer.net>"
 __credits__ = ["Sebastien Celles"]
 __license__ = ""
-__version__ = "1.4.0"
 __email__ = "gustavo@niemeyer.net"
 __status__ = "Development"
 __url__ = "https://github.com/python-constraint/python-constraint"
