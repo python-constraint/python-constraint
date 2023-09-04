76789319725b450acf89beae2fb16801a72b285d
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2023-09-04 10:30:07 +0200

    Made Nox tests more efficient by only running coverage reports on the last session

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,8 @@
 """Configuration file for the Nox test runner.
 
 This instantiates the specified sessions in isolated environments and runs the tests.
-This allows for locally mirroring the testing occuring with GitHub-actions.
-Be careful that the general setup of tests is left to pyproject.toml.
+These tests are ran on GitHub, and can be executed locally by running `nox`.
+Be aware that the general setup of tests is left to pyproject.toml.
 """
 
 
@@ -11,6 +11,7 @@ from nox import Session, session
 
 # from nox_poetry import Session, session   # nox_poetry is a better option, but <=1.0.3 has a bug with filename-URLs
 
+python_versions_to_test = ["3.9", "3.10", "3.11"]
 nox.options.stop_on_first_error = True
 nox.options.error_on_missing_interpreters = True
 
@@ -24,7 +25,7 @@ def lint(session: Session) -> None:
 
 
 # Test code compatiblity and coverage
-@session(python=["3.9", "3.10", "3.11"])  # missing versions can be installed with `pyenv install ...`
+@session(python=python_versions_to_test)  # missing versions can be installed with `pyenv install ...`
 # do not forget check / set the versions with `pyenv global`, or `pyenv local` in case of virtual environment
 def tests(session: Session) -> None:
     """Run the tests for the specified Python versions."""
@@ -34,12 +35,13 @@ def tests(session: Session) -> None:
     # session.poetry.installroot(distribution_format="sdist")
 
     # run pytest on the package with C-extensions, disable required coverage percentage
-    session.run("python", "tests/setup_teardown.py", "--enable_extensions")
     session.run("pytest", "--no-cov")
 
-    # run pytest on the package without C-extensions to generate the correct coverage report
-    session.run("python", "tests/setup_teardown.py", "--no-enable_extensions")
-    session.run("pytest")
+    # for the last Python version session:
+    if session.python == python_versions_to_test[-1]:
+        # run pytest on the package without C-extensions to generate the correct coverage report
+        session.run("python", "tests/setup_teardown.py", "--no-enable_extensions")
+        session.run("pytest")
 
-    # re-enable C-extensions
-    session.run("python", "tests/setup_teardown.py", "--enable_extensions")
+        # re-enable C-extensions
+        session.run("python", "tests/setup_teardown.py", "--enable_extensions")
