af7a0376add9e9b166952888356525d71efbe71b
fjwillemsen <fjwillemsen@icloud.com>
2025-02-11 15:10:00 +0100

    Removed redundant parser tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,22 +5,8 @@ def test_parse_restrictions():
     tune_params = {"x": [50, 100], "y": [0, 1]}
     restrictions = ["x != 320", "y == 0 or x % 32 != 0", "50 <= x * y < 100"]
 
-    # test the split parsed function
-    parsed_multi = parse_restrictions(restrictions, tune_params, try_to_constraint=False)
-    assert isinstance(parsed_multi, list) and isinstance(parsed_multi[0], tuple)
-    assert len(parsed_multi) == 3
-    parsed, params = parsed_multi[0]
-    assert restrictions[0] in parsed
-    assert params == ["x"]
-    parsed, params = parsed_multi[1]
-    assert restrictions[1] in parsed
-    assert all(param in tune_params for param in params)
-    parsed, params = parsed_multi[2]
-    assert restrictions[2] in parsed
-    assert all(param in tune_params for param in params)
-
     # test the conversion to constraints
-    parsed_multi_constraints = parse_restrictions(restrictions, tune_params, try_to_constraint=True)
+    parsed_multi_constraints = parse_restrictions(restrictions, tune_params)
     assert isinstance(parsed_multi_constraints, list) and isinstance(parsed_multi_constraints[0], tuple)
     assert len(parsed_multi_constraints) == 4
     parsed, params = parsed_multi_constraints[0]
@@ -40,11 +26,11 @@ def test_parse_restrictions():
     rw_tune_params = dict()
     rw_tune_params["x"] = [1, 2, 3, 4, 5, 6, 7, 8]
     rw_tune_params["y"] = [1, 2, 3, 4, 5, 6, 7, 8]
-    parsed_constraint, params_constraint = parse_restrictions(["x*y<30"], rw_tune_params, try_to_constraint=True)[0]
+    parsed_constraint, params_constraint = parse_restrictions(["x*y<30"], rw_tune_params)[0]
     assert all(param in rw_tune_params for param in params_constraint)
     assert isinstance(parsed_constraint, MaxProdConstraint)
     assert parsed_constraint._maxprod == 29
-    parsed_constraint, params_constraint = parse_restrictions(["30<x*y"], rw_tune_params, try_to_constraint=True)[0]
+    parsed_constraint, params_constraint = parse_restrictions(["30<x*y"], rw_tune_params)[0]
     assert all(param in rw_tune_params for param in params_constraint)
     assert isinstance(parsed_constraint, MinProdConstraint)
     assert parsed_constraint._minprod == 31
