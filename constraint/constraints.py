6843eebb394a92488d92b4bf1be695336fb92b84
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2025-06-05 18:21:57 +0200

    Implemented the VariableMaxSumConstraint

--- constraint/constraints.py
+++ constraint/constraints.py
@@ -646,7 +646,6 @@ class VariableMinSumConstraint(Constraint):
         if not multipliers:
             for var in self.sum_vars:
                 domain = domains[var]
-                others_min = sum(min(domains[v]) for v in self.sum_vars if v != var)
                 others_max = sum(max(domains[v]) for v in self.sum_vars if v != var)
                 for value in domain[:]:
                     if value + others_max < min(domains[self.target_var]):
@@ -678,6 +677,76 @@ class VariableMinSumConstraint(Constraint):
             sum_value = round(sum_value, 10)
 
         return sum_value >= target_value
+
+class VariableMaxSumConstraint(Constraint):
+    """Constraint enforcing that the sum of variables sum at most to the value of another variable.
+
+    Example:
+        >>> problem = Problem()
+        >>> problem.addVariables(["a", "b", "c"], [1, 3, 4])
+        >>> problem.addConstraint(VariableMaxSumConstraint('c', ['a', 'b']))
+        >>> sorted(sorted(x.items()) for x in problem.getSolutions())
+        [[('a', 1), ('b', 1), ('c', 3)], [('a', 1), ('b', 1), ('c', 4)], [('a', 1), ('b', 3), ('c', 4)], [('a', 3), ('b', 1), ('c', 4)]]
+    """
+
+    def __init__(self, target_var: str, sum_vars: Sequence, multipliers: Optional[Sequence] = None):
+        """Initialization method.
+
+        Args:
+            target_var (Variable): The target variable to sum to.
+            sum_vars (sequence of Variables): The variables to sum up.
+            multipliers (sequence of numbers): If given, variable values
+                (except the last) will be multiplied by the given factors before being
+                summed to match the last variable.
+        """
+        self.target_var = target_var
+        self.sum_vars = sum_vars
+        self._multipliers = multipliers
+
+        if multipliers:
+            assert len(multipliers) == len(sum_vars) + 1, "Multipliers must match sum variables and +1 for target."
+            assert all(isinstance(m, (int, float)) for m in multipliers), "Multipliers must be numbers."
+            assert multipliers[-1] == 1, "Last multiplier must be 1, as it is the target variable."
+
+    def preProcess(self, variables: Sequence, domains: dict, constraints: list[tuple], vconstraints: dict):     # noqa: D102
+        Constraint.preProcess(self, variables, domains, constraints, vconstraints)
+
+        multipliers = self._multipliers
+
+        if not multipliers:
+            for var in self.sum_vars:
+                domain = domains[var]
+                others_min = sum(min(domains[v]) for v in self.sum_vars if v != var)
+                for value in domain[:]:
+                    if value + others_min > max(domains[self.target_var]):
+                        domain.remove(value)
+
+    def __call__(self, variables: Sequence, domains: dict, assignments: dict, forwardcheck=False):      # noqa: D102
+        multipliers = self._multipliers
+
+        if self.target_var not in assignments:
+            return True  # can't evaluate without target, defer to later
+
+        target_value = assignments[self.target_var]
+        sum_value = 0
+
+        if multipliers:
+            for var, multiplier in zip(self.sum_vars, multipliers):
+                if var in assignments:
+                    sum_value += assignments[var] * multiplier
+                else:
+                    sum_value += min(domains[var] * multiplier)  # use min value if not assigned
+        else:
+            for var in self.sum_vars:
+                if var in assignments:
+                    sum_value += assignments[var]
+                else:
+                    sum_value += min(domains[var])  # use min value if not assigned
+
+        if isinstance(sum_value, float):
+            sum_value = round(sum_value, 10)
+
+        return sum_value <= target_value
 
 
 class MaxProdConstraint(Constraint):
